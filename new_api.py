d1179d9be585d3bf1325e99304256e28e8ad7063
Matthew Margelony <mastermargie@gmail.com>
2020-01-07 13:49:00 -0500

    Testing variable usage from CLI

--- new_api.py
+++ new_api.py
@@ -51,7 +51,7 @@ class veracode_api_call():
 		logging.getLogger("requests").setLevel(logging.WARNING)
 		logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(message)s")
 		self.logger = logging.getLogger()
-		self.logger.setLevel(logging.INFO)
+		self.logger.setLevel(logging.INFO) #
 
 		fileHandler = logging.FileHandler("{0}/{1}.log".format(os.getcwd(), 'logs'))
 		fileHandler.setFormatter(logFormatter)
