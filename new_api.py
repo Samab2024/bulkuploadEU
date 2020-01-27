008c982c567c4564fade39dedab1d0c72066ded2
Matthew Margelony <mastermargie@gmail.com>
2020-01-27 15:33:42 -0500

    PATCH: Better error handling around bad credentials

--- new_api.py
+++ new_api.py
@@ -1,4 +1,4 @@
-import requests, sys, os, json, logging, logger
+import requests, logger
 import xml.etree.ElementTree as ET
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC as VeracodeHMAC
 
@@ -9,23 +9,18 @@ class veracode_api_call():
 		try: self.rownum = 'Line #%s' % (params.pop('rownum'))
 		except: self.rownum = 'TEST'
 
-		self.url, self.response_type = self.get_url_from_endpoint(endpoint)
+		self.url = self.get_url_from_endpoint(endpoint)
 
 		if creds == None:
 			self.auth=VeracodeHMAC(profile = None)
 		else: self.auth=(creds)
 
 		self.r = requests.get(url=self.url, auth=self.auth, params=params)
-		
-		if self.response_type == 'json':
-			self.response = json.loads( str( self.r.text ) )
-		else:
-			self.response = ET.fromstring( self.r.text )
 
 		self.log_activity()
 
 	def get_url_from_endpoint(self, endpoint):
-		version_numbers = dict(beginprescan='5.0', beginscan='5.0', createapp='4.0', \
+		xml_version_numbers = dict(beginprescan='5.0', beginscan='5.0', createapp='4.0', \
 			createbuild='4.0', deleteapp='5.0', deletebuild='5.0', getappinfo='5.0', \
 			getapplist='5.0', getbuildinfo='5.0', getbuildlist='5.0', getfilelist='5.0', \
 			getpolicylist='5.0', getprescanresults='5.0', getvendorlist='5.0', \
@@ -39,16 +34,32 @@ class veracode_api_call():
 			getteamlist='3.0', updateteam='3.0', getcurriculumlist='3.0', gettracklist='3.0', \
 			getmaintenancescheduleinfo='3.0', generateflawreport='3.0', downloadflawreport='3.0')
 
-		if endpoint in version_numbers:
-			return '%s/%s/%s.do' % ('https://analysiscenter.veracode.com/api', version_numbers[endpoint], endpoint), 'xml'
-
+		if endpoint in xml_version_numbers:
+			return '%s/%s/%s.do' % ('https://analysiscenter.veracode.com/api', xml_version_numbers[endpoint], endpoint)
 		else:
-			return '%s/%s' % ('https://api.veracode.io/elearning/v1/', endpoint), 'json'
-
-	def get_response(self): return self.response
+			return '%s/%s' % ('https://api.veracode.io/elearning/v1/', endpoint)
 
 	def log_activity(self):
-		if self.response.tag == 'error':
-			self.logger.error( "[{0}]{1}".format(self.rownum, self.response.text) )
+		self.r.raise_for_status()
+		if self.r.status_code == 401:
+			self.logger.error( "[{0}]{1}".format(self.rownum, "401 Unauthorized") )
+		elif 'text/xml' in self.r.headers.get('content-type'):
+			response = ET.fromstring(self.r.content)
+			if response.tag == 'error':
+				self.logger.error( "[{0}]{1}".format(self.rownum, response.text) )
+			else:
+				self.logger.info( "[{0}]{1}".format(self.rownum, 'Success')	)
+		elif 'application/json' in self.r.headers.get('content-type'):
+			self.logger.info( "[{0}]{1}".format(self.rownum, 'Unsure how to process JSON') )
 		else:
-			self.logger.info( "[{0}]{1}".format(self.rownum, 'Success')	)
+			self.logger.info( "[{0}]{1}".format(self.rownum, 'Unsure how to process response') )
+
+	def print_response_info(self):
+		print '>>> r.status_code'
+		print self.r.status_code
+		print '>>> r.encoding'
+		print self.r.encoding
+		print ">>> r.headers.get('content-type')"
+		print self.r.headers.get('content-type')
+		print '>>> r.text'
+		print str(self.r.text)
